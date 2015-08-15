1ddeb52c0e0c8a11a4bdb7c9a6263476b50ad21e
Heark <hearkofsorcerey@gmail.com>
2015-08-15 03:10:28 -0400

    Update build.js

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 /* 
 Candy JS
 */
-(function(Candy) {
     var Candy = this.Candy = {};
     Candy.httpGet = function(url) {
         function http(URL) {
@@ -10,8 +9,8 @@ Candy JS
             candyHttp.send(null);
             return candyHttp.responseText;
         }
-        return http(this.url)
-    }
+        return http(this.url);
+    };
 
     // Candy.httpGet( URL )
 
@@ -20,25 +19,25 @@ Candy JS
         this.contents = contents;
         var output = this.contents[Math.floor(Math.random() * this.contents.length)];
         return output;
-    }
+    };
 
     // Candy.shuffle( Array )
 
     Candy.rand = function(arg, arg_2) {
 
             var output = Math.floor(Math.random() * (arg - arg_2 + 1)) + arg_2;
-            return output
-        }
+            return output;
+        };
         // Candy.rand( First Number, Second Number 
     Candy.toVar = function(arg, contains) {
         arg = this.arg;
         contains = this.contains;
-        window[arg] = contains
-    }
+        window[arg] = contains;
+    };
 
     Candy.write = function(arg, arg2) {
         if (this.arg2 == true) {
-            console.log(this.arg)
+            console.log(this.arg);
         } else if (this.arg2 == undefined) {
             var w = document.createElement("div");
             w.textContent = this.arg;
@@ -49,32 +48,32 @@ Candy JS
             document.body.appendChild(w);
 
         }
-    }
+    };
     Candy.time = function() {
-            var d = new Date;
+            var d = new Date();
             var hour = d.getHours() == 0 ? 12 : (d.getHours() > 12 ? d.getHours() - 12 : d.getHours());
             var min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
             var ampm = d.getHours() < 12 ? 'AM' : 'PM';
             var candy_time = hour + ':' + min + ' ' + ampm;
-            return candy_time
+            return candy_time;
 
-        }
+        };
         // Candy.time()
     Candy.contains = function(array, value) {
         this.array = array;
         this.value = value;
         if (this.array.indexOf(this.value) !== -1) {
-            return true
+            return true;
         } else {
-            return false
+            return false;
         }
-    }
+    };
     Candy.newList = function(items, tags) {
-            this.tags = tags
-            this.items = items
+            this.tags = tags;
+            this.items = items;
 
             // Establish the array which acts as a data source for the list
-            var listData = this.items
+            var listData = this.items;
 
             // Make a container element for the list - which is a <div>
             // You don't actually need this container to make it work
@@ -84,7 +83,7 @@ Candy JS
             document.getElementsByTagName("body")[0].appendChild(listContainer);
 
             // Make the list itself which is a <ul>
-            var listElement = document.createElement(this.tag[0]);
+            var listElement = document.createElement(this.tags[0]);
 
             // add it to the page
             listContainer.appendChild(listElement);
@@ -105,14 +104,13 @@ Candy JS
 
             }
 
-        }
+        };
         // Only Works with HTML... Candy.newMenu(["JAN", "FEB"], ["ul", "li"])
 
-    Candy.getIP = function(cache) {
+    Candy.getIP = function() {
 
         return '<!--#echo var="REMOTE_ADDR"-->';
-    }
+    };
 
     // Only Works with HTML... Candy.getIP()
-})(Candy)
-console.log()
+
