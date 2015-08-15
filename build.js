df0066cfbfe0cd91219f625a013bd5bbdb099b6d
Heark <hearkofsorcerey@gmail.com>
2015-08-15 04:38:40 -0400

    Update build.js

--- build.js
+++ build.js
@@ -1,131 +1,114 @@
 /* 
 Candy JS
-*/
-var Candy = this.Candy = {};
+ */
+var Candy;
+
+Candy = this.Candy = {};
+
 Candy.httpGet = function(url) {
-    var xmlhttp;
-    this.url = url;
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  var output, xmlhttp;
+  xmlhttp = void 0;
+  this.url = url;
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest;
+  } else {
+    xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+  }
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState === XMLHttpRequest.DONE) {
+      if (xmlhttp.status === 200) {
+        document.getElementById('myDiv').innerHTML = xmlhttp.responseText;
+      } else if (xmlhttp.status === 400) {
+        alert('There was an error 400');
+      } else {
+        console.log('Error something else other than 200 was returned');
+      }
     }
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-            if (xmlhttp.status == 200) {
-                document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-            } else if (xmlhttp.status == 400) {
-                alert('There was an error 400')
-            } else {
-                console.log('Error something else other than 200 was returned')
-            }
-        }
-    }
-
-    var output = xmlhttp.open("GET", this.url, true);
-    xmlhttp.send();
-    return output
+  };
+  output = xmlhttp.open('GET', this.url, true);
+  xmlhttp.send();
+  return output;
 };
-
-// Candy.httpGet( URL )
-
 
 Candy.shuffle = function(contents) {
-    this.contents = contents;
-    var output = this.contents[Math.floor(Math.random() * this.contents.length)];
-    return output;
+  var output;
+  this.contents = contents;
+  output = this.contents[Math.floor(Math.random() * this.contents.length)];
+  return output;
 };
-
-// Candy.shuffle( Array )
 
 Candy.rand = function(arg, arg_2) {
-
-    var output = Math.floor(Math.random() * (arg - arg_2 + 1)) + arg_2;
-    return output;
+  var output;
+  output = Math.floor(Math.random() * (arg - arg_2 + 1)) + arg_2;
+  return output;
 };
-// Candy.rand( First Number, Second Number 
+
 Candy.toVar = function(arg, contains) {
-    arg = this.arg;
-    contains = this.contains;
-    window[arg] = contains;
+  arg = this.arg;
+  contains = this.contains;
+  window[arg] = contains;
 };
 
 Candy.write = function(arg, arg2) {
-    if (this.arg2 == true) {
-        console.log(this.arg);
-    } else if (this.arg2 == undefined) {
-        var w = document.createElement("div");
-        w.textContent = this.arg;
-        document.body.appendChild(w);
-    } else {
-        var w = document.createElement("div");
-        w.textContent = this.arg;
-        document.body.appendChild(w);
-
-    }
+  var w;
+  var w;
+  if (this.arg2 === true) {
+    console.log(this.arg);
+  } else if (this.arg2 === void 0) {
+    w = document.createElement('div');
+    w.textContent = this.arg;
+    document.body.appendChild(w);
+  } else {
+    w = document.createElement('div');
+    w.textContent = this.arg;
+    document.body.appendChild(w);
+  }
 };
+
 Candy.time = function() {
-    var d = new Date();
-    var hour = d.getHours() == 0 ? 12 : (d.getHours() > 12 ? d.getHours() - 12 : d.getHours());
-    var min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-    var ampm = d.getHours() < 12 ? 'AM' : 'PM';
-    var candy_time = hour + ':' + min + ' ' + ampm;
-    return candy_time;
-
+  var ampm, candy_time, d, hour, min;
+  d = new Date;
+  hour = d.getHours() === 0 ? 12 : d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+  min = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+  ampm = d.getHours() < 12 ? 'AM' : 'PM';
+  candy_time = hour + ':' + min + ' ' + ampm;
+  return candy_time;
 };
-// Candy.time()
+
 Candy.contains = function(array, value) {
-    this.array = array;
-    this.value = value;
-    if (this.array.indexOf(this.value) !== -1) {
-        return true;
-    } else {
-        return false;
-    }
+  this.array = array;
+  this.value = value;
+  if (this.array.indexOf(this.value) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
 Candy.newList = function(items, tags) {
-    this.tags = tags;
-    this.items = items;
-
-    // Establish the array which acts as a data source for the list
-    var listData = this.items;
-
-    // Make a container element for the list - which is a <div>
-    // You don't actually need this container to make it work
-    var listContainer = document.createElement("div");
-
-    // add it to the page
-    document.getElementsByTagName("body")[0].appendChild(listContainer);
-
-    // Make the list itself which is a <ul>
-    var listElement = document.createElement(this.tags[0]);
-
-    // add it to the page
-    listContainer.appendChild(listElement);
-
-    // Set up a loop that goes through the items in listItems one at a time
-    var numberOfListItems = listData.length;
-
-    for (var i = 0; i < numberOfListItems; ++i) {
-
-        // create a <li> for each one.
-        var listItem = document.createElement(this.tags[1]);
-
-        // add the item text
-        listItem.innerHTML = listData[i];
-
-        // add listItem to the listElement
-        listElement.appendChild(listItem);
-
-    }
-
+  var i, listContainer, listData, listElement, listItem, numberOfListItems;
+  this.tags = tags;
+  this.items = items;
+  listData = this.items;
+  listContainer = document.createElement('div');
+  document.getElementsByTagName('body')[0].appendChild(listContainer);
+  listElement = document.createElement(this.tags[0]);
+  listContainer.appendChild(listElement);
+  numberOfListItems = listData.length;
+  i = 0;
+  while (i < numberOfListItems) {
+    listItem = document.createElement(this.tags[1]);
+    listItem.innerHTML = listData[i];
+    listElement.appendChild(listItem);
+    ++i;
+  }
 };
-// Only Works with HTML... Candy.newMenu(["JAN", "FEB"], ["ul", "li"])
 
 Candy.getIP = function() {
-
-    return '<!--#echo var="REMOTE_ADDR"-->';
+  return '<!--#echo var="REMOTE_ADDR"-->';
 };
 
-// Only Works with HTML... Candy.getIP()
+console.log(Candy.time());
+
+// ---
+// generated by coffee-script 1.9.2
