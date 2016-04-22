70b529b8425b683d4a67e736165297641350c476
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-04-22 13:52:11 -0400

    Update anagram.js

--- examples/anagram.js
+++ examples/anagram.js
@@ -1,1 +1,1 @@
-var output=[],split_word=[],anagram=function(r){for(var t=0;t<r.length;t++)split_word.push(r[t]);for(var o=0;o<split_word.length;o++)output.push(Peanut.shuffle(split_word[o]));return output.reverse().sort(Peanut.rand(1,100)).join("")};console.log(anagram("java"));
+var output=[],split_word=[],anagram=function(t){function r(t){for(var r,o,u=t.length;0!==u;)o=Math.floor(Math.random()*u),u-=1,r=t[u],t[u]=t[o],t[o]=r;return t}for(var o=0;o<t.length;o++)split_word.push(t[o]);for(var u=0;u<split_word.length;u++)output.push(Peanut.shuffle(split_word[u]));return r(output).join("")};
